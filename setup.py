269632ef832ba749e65a0e9decaae3f4ed110d5c
Double_V <liuvv0203@163.com>
2021-06-29 15:49:34 +0800

    add install requires

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup
 # python3.7 setup.py bdist_wheel
+
+with open('requirements.txt', encoding="utf-8-sig") as f:
+    requirements = f.readlines()
+    requirements.append('tqdm')
+
+      
 setup(name='AutoLog',
       version='1.0.0',
+      install_requires=requirements,
       description='Auto logger',
       author='Liuweiwei',
       author_email='liuvv0203@gmail.com',
